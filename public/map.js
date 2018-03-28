a8d1dc27f3005ae7f734564003fdc8dd275631cc
Mackenzie Craig <maccraig98@gmail.com>
2018-03-28 11:16:59 -0700

    Add custom pretty map

--- public/map.js
+++ public/map.js
@@ -13,13 +13,172 @@ function clearOverlays() {
 }
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 17,
+
+  var mapOptions = {
+
+    zoom: 14,
     center: {
       lat: 49.249889599999996,
       lng: -123.00167519999998
-    }
-  });
+    },
+
+    styles: [{
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [{
+          "color": "#444444"
+        }]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [{
+          "color": "#f2f2f2"
+        }]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "visibility": "on"
+          },
+          {
+            "hue": "#ff0000"
+          }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry",
+        "stylers": [{
+          "lightness": "100"
+        }]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "lightness": "100"
+        }]
+      },
+      {
+        "featureType": "landscape.natural",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "landscape.natural.landcover",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "visibility": "on"
+        }]
+      },
+      {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "geometry",
+        "stylers": [{
+          "lightness": "100"
+        }]
+      },
+      {
+        "featureType": "landscape.natural.terrain",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "visibility": "off"
+          },
+          {
+            "lightness": "23"
+          }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [{
+            "saturation": -100
+          },
+          {
+            "lightness": 45
+          }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [{
+          "visibility": "simplified"
+        }]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [{
+          "color": "#ffd900"
+        }]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      },
+      {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [{
+            "color": "#ffd900"
+          },
+          {
+            "visibility": "on"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [{
+            "visibility": "on"
+          },
+          {
+            "color": "#cccccc"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [{
+          "visibility": "off"
+        }]
+      }
+    ]
+  };
+
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
   // marker = new google.maps.Marker({
   //   position: {
   //     lat: x,
